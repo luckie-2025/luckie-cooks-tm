02bb4ff1fa63f52cd1dddbfe1537581c83686c36
Luckie <zubiyafatima2025@gmail.com>
2026-05-21 19:34:15 +0530

    Add episodes data array to data.js

---
+++ data.js
@@ -0,0 +1,17 @@
+const episodes = [
+  {
+    id: 1,
+    title: "Episode 1",
+    desc: "Sweet basics & honey toast"
+  },
+  {
+    id: 2,
+    title: "Episode 2",
+    desc: "Blueberry drinks"
+  },
+  {
+    id: 3,
+    title: "Episode 3",
+    desc: "Knife skills & sauces"
+  }
+];
